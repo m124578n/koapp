062816e446c38249bb73109435feec8bd9443105
m124578n <m23568n@gmail.com>
2026-06-05 20:52:49 +0800

    feat: home screen with bottom navigation shell

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,13 +1,44 @@
 // lib/presentation/screens/home/home_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/l10n/app_localizations.dart';
+import '../study/deck_selection_screen.dart';
+import '../library/library_screen.dart';
+import '../progress/progress_screen.dart';
+import '../settings/settings_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _currentIndex = 0;
+
+  static const _screens = [
+    DeckSelectionScreen(),
+    LibraryScreen(),
+    ProgressScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Home')),
+    final l = AppLocalizations.of(context)!;
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (i) => setState(() => _currentIndex = i),
+        destinations: [
+          NavigationDestination(icon: const Icon(Icons.school), label: l.tabStudy),
+          NavigationDestination(icon: const Icon(Icons.library_books), label: l.tabLibrary),
+          NavigationDestination(icon: const Icon(Icons.bar_chart), label: l.tabProgress),
+          NavigationDestination(icon: const Icon(Icons.settings), label: l.tabSettings),
+        ],
+      ),
     );
   }
 }
---
+++ lib/presentation/screens/study/deck_selection_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class DeckSelectionScreen extends StatelessWidget {
+  const DeckSelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('Study')));
+}
